bb61b7cde5f6336a05e6e4e104df84a900891e37
Victor Socaci <Victor.Socaci@endava.com>
2025-11-20 17:53:19 +0200

    Nu am terminat un test dar daca mai aveam 10 min il dovedeam si pe ala

--- src/main/java/com/bvd/java_fundamentals/OrderUtil.java
+++ src/main/java/com/bvd/java_fundamentals/OrderUtil.java
@@ -4,10 +4,8 @@ import com.bvd.java_fundamentals.model.Order;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /*
  * Implement the methods below so that the requirements are met.
@@ -19,32 +17,73 @@ public class OrderUtil {
 
     // retrieve orders from csv lines
     public static List<Order> parseCsvLines(final List<String> lines) {
-        // Write your code here and replace the return statement
-        return Collections.emptyList();
+        if (lines == null)
+            return Collections.emptyList();
+        return lines.stream()
+                .map(field -> field.split(","))
+                .filter(s -> s.length == 7)
+                .map(ord -> new Order(
+                        ord[0].trim(),
+                        ord[1].trim(),
+                        LocalDate.parse(ord[2]),
+                        ord[3].trim(),
+                        ord[4].trim(),
+                        new BigDecimal(ord[5]),
+                        Integer.parseInt(ord[6])
+                ))
+                .toList();
     }
 
     // calculate revenue by day
     // revenue = unitPrice * quantity
     public static Map<LocalDate, BigDecimal> revenueByDay(final List<Order> orders) {
-        // Write your code here and replace the return statement
-        return Collections.emptyMap();
+        return orders.stream()
+                .collect(Collectors.groupingBy(
+                        Order::getOrderDate,
+                        Collectors.reducing(BigDecimal.ZERO,
+                                Order::getRevenue,
+                                BigDecimal::add)
+                ));
+
     }
 
     // get top "n" products by revenue
     public static List<Map.Entry<String, BigDecimal>> topProductsByRevenue(final List<Order> orders, final int n) {
-        // Write your code here and replace the return statement
-        return Collections.emptyList();
+        Map<String, BigDecimal> productsWithRevenue = orders.stream()
+                .collect(Collectors.groupingBy(
+                        Order::getProductName,
+                        Collectors.mapping(Order::getRevenue, Collectors.reducing(BigDecimal.ZERO, BigDecimal::add)
+                        )
+                ));
+                return productsWithRevenue.entrySet().stream()
+                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                        .toList();
     }
 
     // get customers who ordered products from at least "minCategories" different categories
     public static List<String> customersWithCategoryDiversity(final List<Order> orders, final int minCategories) {
-        // Write your code here and replace the return statement
-        return Collections.emptyList();
+        Map<String, Long> productsWithRevenue = orders.stream()
+                .collect(Collectors.groupingBy(
+                        Order::getCustomerId,
+                        Collectors.counting()
+                        )
+                );
+
+        // In loc de counting vreau sa fac un set cu categoriile si dupa dadeam count la el sa vad cate categorii au si
+        // si daca aveau min filtram acel set intr o lista si o returnam
+        List<String> customers= productsWithRevenue.entrySet()
+                .stream().filter(e -> e.getValue() > minCategories)
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList());
+
+        System.out.println("productsWithRevenue: " + customers);
+        return customers;
     }
 
     // find the first product containing a given substring (case-insensitive)
     public static Optional<Order> findFirstProductContaining(final List<Order> orders, final String product) {
-        // Write your code here and replace the return statement
-        return Optional.empty();
+        return orders.stream()
+                .filter(order -> order.getProductName().toLowerCase().contains(product.toLowerCase()))
+                .findFirst();
     }
 }
