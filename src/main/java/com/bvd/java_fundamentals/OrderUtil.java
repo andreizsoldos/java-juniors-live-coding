45ec65e1fc6c5a1593531d3b5e2e521588a181d4
virgil <cimpean.virgil2@gmail.com>
2025-11-25 15:01:56 +0200

    commit

--- src/main/java/com/bvd/java_fundamentals/OrderUtil.java
+++ src/main/java/com/bvd/java_fundamentals/OrderUtil.java
@@ -4,10 +4,8 @@ import com.bvd.java_fundamentals.model.Order;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /*
  * Implement the methods below so that the requirements are met.
@@ -19,20 +17,47 @@ public class OrderUtil {
 
     // retrieve orders from csv lines
     public static List<Order> parseCsvLines(final List<String> lines) {
-        // Write your code here and replace the return statement
-        return Collections.emptyList();
+        List<Order> orders = new ArrayList<>();
+        for (String line : lines) {
+            try{
+                String[] parts = line.split(",");
+
+                if(parts.length != 7){
+                    continue;
+                }
+                String orderId = parts[0];
+                String productId = parts[1];
+                LocalDate date = LocalDate.parse(parts[2]);
+                String productName = parts[3].trim();
+                String category = parts[4].trim();
+                BigDecimal unitPrice = new BigDecimal(parts[5].trim());
+                int quantity = Integer.parseInt(parts[6].trim());
+
+                Order order = new Order(orderId, productId, date, productName, category, unitPrice, quantity);
+                orders.add(order);
+            } catch (Exception e) {
+
+            }
+
+        }
+        return orders;
     }
 
     // calculate revenue by day
     // revenue = unitPrice * quantity
     public static Map<LocalDate, BigDecimal> revenueByDay(final List<Order> orders) {
-        // Write your code here and replace the return statement
-        return Collections.emptyMap();
+        return orders.stream()
+                .collect(Collectors.groupingBy(
+                        order -> order.getOrderDate(),
+                        Collectors.summingDouble( order -> order.getRevenue())
+                ));
+
+
     }
 
     // get top "n" products by revenue
     public static List<Map.Entry<String, BigDecimal>> topProductsByRevenue(final List<Order> orders, final int n) {
-        // Write your code here and replace the return statement
+
         return Collections.emptyList();
     }
 
